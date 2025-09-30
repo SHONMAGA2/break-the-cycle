0ba8ea5cf0052e4b1d4518d43fa9634255fff6d0
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-09-30 13:08:18 +0200

    Update app.ts

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express,{Application} from 'express';
+import path from 'path';
+
+const app: Application=express();
+
+app.use(express.static(path.join(__dirname,"../public")));
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+import homeRoute from './route/home';
+
+app.use("/home",homeRoute);
+
+export default app;
