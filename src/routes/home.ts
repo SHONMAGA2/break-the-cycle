9d9ba720cf5fd865d81fbaebee050e5cb31e2949
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-09-30 13:39:09 +0200

    Update home.ts

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,8 +3,6 @@ import {Request,Response} from 'express';
 
 const router = express.Router();
 
-router.set("view engine","ejs");
-
 router.get("/",(req: Request,res: Response) =>{
 res.render("home",{
 
