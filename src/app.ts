2423eefc3546d6b90949184b7c461000b8d43335
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-09-30 13:35:47 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname,"../public")));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
-import homeRoute from './route/home.ts';
+import homeRoute from './route/home';
 
 app.use("/home",homeRoute);
 
