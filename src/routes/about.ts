5a7025a629cc4005c133e9becba57aaf0503e26f
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-09-30 20:33:40 +0200

    Update about.ts

---
+++ src/routes/about.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import {Request,Response} from 'express';
+
+const router = express.Router();
+
+router.get("/",(req: Request,res: Response) =>{
+res.render("home",{
+
+title:"About | Break The Cycle",
+
+description:"Youth-led initiative tackling cyber addiction, plagiarism, and underperformance.",
+
+active:"home"});
+
+});
+
+export default router;
