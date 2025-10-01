d810533dbe5d872b195ec722bf9c7d10f5e5d4cd
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-10-01 23:15:01 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,11 @@ app.set("views",path.join(__dirname,"views"));
 import homeRoute from './routes/home';
 import aboutRoute from './routes/about';
 import problemsRoute from './routes/problems';
+import solutionsRoute from './routes/solutions';
 
 app.use("/",homeRoute);
 app.use("/about",aboutRoute);
 app.use("/problems",problemsRoute);
+app.use("/solutions",solutionsRoute);
 
 export default app;
