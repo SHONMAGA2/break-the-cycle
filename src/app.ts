61eb428ae708d2163c7466deae643d86ab6f0f54
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-10-02 02:00:42 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app: Application=express();
 
 app.use(express.static(path.join(__dirname,"../public")));
 app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+app.set("views",path.join(__dirname,"../src/views"));
 
 import homeRoute from './routes/home';
 import aboutRoute from './routes/about';
